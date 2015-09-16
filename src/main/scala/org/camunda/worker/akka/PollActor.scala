1b0d1d5602559db2341784a553bee920fe57e19a
Philipp Ossler <philipp.ossler@camunda.com>
2015-09-16 16:42:22 +0200

    refactoring

--- src/main/scala/org/camunda/worker/akka/PollActor.scala
+++ src/main/scala/org/camunda/worker/akka/PollActor.scala
@@ -1,114 +1,111 @@
 package org.camunda.worker.akka
 
-/**
- * @author Philipp Ossler
- */
-
-import akka.actor.{Actor, ActorRef, ActorLogging, Props}
+import akka.actor.{ Actor, ActorRef, ActorLogging, Props }
 import akka.pattern.ask
 import akka.util.Timeout
 import scala.concurrent.duration._
-import org.springframework.web.client.RestTemplate
-import org.camunda.worker.dto._
-import scala.math.{min, max, round}
-import scala.collection.JavaConversions.{seqAsJavaList, asScalaBuffer}
-import scala.concurrent._
-import org.camunda.worker.akka.client.CamundaClientActor.{PollRequest, TaskCompleted, TaskFailed}
-import org.camunda.worker.akka.client.LockedTask
-import org.camunda.worker.akka.client.CamundaClientActor
-import org.camunda.worker.akka.client.CamundaClientActor.PollRequest
-import org.camunda.worker.akka.client.PollAndLockTaskRequest
-import scala.util.{Success, Failure}
-import org.camunda.worker.akka.client.CamundaClientActor.TaskCompleted
-import org.camunda.worker.akka.client.CompleteTaskRequest
-import org.camunda.worker.akka.client.CamundaClientActor.TaskFailed
-import org.camunda.worker.akka.client.CamundaClientActor.TaskFailed
-import org.camunda.worker.akka.client.FailedTaskRequest
-import akka.actor.PoisonPill
-import akka.actor.Kill
-import scala.util.Either
-import org.camunda.worker.akka.client.VariableValue
+import scala.concurrent.Future
+import scala.util.{ Success, Failure }
 
+import org.camunda.worker.akka.client.CamundaClientActor.{ PollRequest, TaskCompleted, TaskFailed }
+import org.camunda.worker.akka.client._
+import org.camunda.worker.akka.worker.Worker
+
+/**
+ * Coordinate polling of tasks.
+ */
 class PollActor(hostAddress: String, maxTasks: Int, lockTime: Int, waitTime: Int) extends Actor with ActorLogging {
-  
+
   import PollActor._
   import context._
-    
-  val clientActor = context.actorOf(CamundaClientActor.props(hostAddress), name = "client")
-  
-  // timeout for client actor
-  implicit val timeout = Timeout(5000 millis)
-  
+
+  // create actor for communication with camunda server
+  val clientActor = context.actorOf(CamundaClientActor.props(hostAddress), name = "camunda-client")
+  // 'ask' timeout for client actor
+  implicit val timeout = Timeout(5 seconds)
+
   def receive = {
+
     case poll @ Poll(topicName, worker, variableNames, strategy) => {
+
       log.debug(s"poll tasks from server '$hostAddress' with topic '$topicName'")
-      
+      // use the name of the worker as consumerId
+      val consumerId = Worker.getNameOfActor(worker)
       // poll tasks from server
-      val result = clientActor ? PollRequest(request = PollAndLockTaskRequest(topicName, "demo", lockTime, maxTasks, variableNames))
-      result onComplete {
-        case Success(LockedTasks(tasks))        => {
-                                                    if(!tasks.isEmpty) {
-                                                      log.info(s"shedule '${tasks.size}' tasks for topic '$topicName'")
-                                                      // assigne tasks to worker
-                                                      tasks.foreach( task => worker ! task )
-                                                      // calculate wait time
-                                                      strategy.polledTasks
-                                                    } else {
-                                                      log.debug(s"no tasks polled for topic '$topicName'")
-                                                      // calculate wait time
-                                                      strategy.noPolledTasks
-                                                    }
-                                                    log.debug(s"wait '${strategy.waitTime}ms' till poll for topic '$topicName'")
-                                                    // schedule next poll request 
-                                                    system.scheduler.scheduleOnce(strategy.waitTime, self, poll)
-                                                   }
-        case Success(FailedToPollTasks(cause))  => {
-                                                     log.error(cause,s"failed to poll tasks for topic '$topicName'")
-                                                     // calculate wait time
-                                                     strategy.failedToPollTasks
-                                                     // schedule next poll request 
-                                                     system.scheduler.scheduleOnce(strategy.waitTime, self, poll)
-                                                   }
-        case Success(result)                    =>   log.warning(s"unknown result from client actor: $result")
-        case Failure(cause)                     => {
-                                                     log.error(cause,s"failed to poll tasks for topic '$topicName'")
-                                                     // calculate wait time
-                                                     strategy.failedToPollTasks
-                                                     // schedule next poll request 
-                                                     system.scheduler.scheduleOnce(strategy.waitTime, self, poll)
-                                                    }
+      val response: Future[Any] = clientActor ? PollRequest(request = PollAndLockTaskRequest(topicName, consumerId, lockTime, maxTasks, variableNames))
+      response onComplete {
+        case Success(LockedTasks(tasks))       => handleSuccessfulPollRequest(poll, tasks)
+        case Success(FailedToPollTasks(cause)) => handleFailedPollRequest(poll, cause)
+        case Failure(cause)                    => handleFailedPollRequest(poll, cause)
+        case Success(result)                   => log.error(s"unknown result from client actor: $result")
       }
     }
-    
+
     case Complete(consumerId, taskId, variables) => {
+
       log.info(s"task '$taskId' completed by '$consumerId'")
-      // notify server that task is completed
-      clientActor ! TaskCompleted(taskId, CompleteTaskRequest("demo", variables))
-    }      
-      
+      // notify the server that task is executed successful
+      clientActor ! TaskCompleted(taskId, CompleteTaskRequest(consumerId, variables))
+    }
+
     case FailedTask(consumerId, taskId, errorMessage) => {
+
       log.info(s"task '$taskId' failed by '$consumerId'")
-      // notify server that task is failed
-      clientActor ! TaskFailed(taskId, FailedTaskRequest("demo", errorMessage))
+      // notify the server that task is failed
+      clientActor ! TaskFailed(taskId, FailedTaskRequest(consumerId, errorMessage))
     }
   }
-  
-  private def toInt(d: Double): Int = round(d.toFloat)
-  
+
+  private def handleSuccessfulPollRequest(poll: Poll, tasks: List[LockedTask]) {
+    val topicName = poll.topicName
+    val worker = poll.worker
+    val strategy = poll.strategy
+
+    if (!tasks.isEmpty) {
+      log.info(s"shedule '${tasks.size}' tasks for topic '$topicName'")
+      // assign tasks to worker
+      tasks.foreach(task => worker ! task)
+      // calculate wait time
+      strategy.polledTasks
+    } else {
+      log.debug(s"no tasks polled for topic '$topicName'")
+      // calculate wait time
+      strategy.noPolledTasks
+    }
+
+    schedulePollRequest(poll)
+  }
+
+  private def handleFailedPollRequest(poll: Poll, cause: Throwable) {
+    log.error(cause, s"failed to poll tasks for topic '${poll.topicName}'")
+    // calculate wait time
+    poll.strategy.failedToPollTasks
+
+    schedulePollRequest(poll)
+  }
+
+  private def schedulePollRequest(poll: Poll) {
+    val waitTime = poll.strategy.waitTime
+
+    log.debug(s"wait '${waitTime}ms' till poll for topic '${poll.topicName}'")
+    // schedule next poll request 
+    system.scheduler.scheduleOnce(poll.strategy.waitTime, self, poll)
+  }
+
 }
 
 object PollActor {
-  
-  def props(hostAddress: String, maxTasks: Int = 1, lockTime: Int = 60, waitTime: Int = 3000): Props = 
+
+  def props(hostAddress: String, maxTasks: Int = 1, lockTime: Int = 60, waitTime: Int = 3000): Props =
     Props(new PollActor(hostAddress, maxTasks, lockTime, waitTime))
-  
+
   case class Poll(topicName: String, worker: ActorRef, variableNames: List[String] = List(), strategy: PollBackOffStrategy = new PollBackOffStrategy())
-  
+
   case class Complete(consumerId: String, taskId: String, variables: Map[String, VariableValue] = Map())
 
   case class FailedTask(consumerId: String, taskId: String, errorMessage: String)
-  
+
   case class LockedTasks(tasks: List[LockedTask])
-  
+
   case class FailedToPollTasks(cause: Throwable)
 }
--- src/main/scala/org/camunda/worker/akka/worker/Worker.scala
+++ src/main/scala/org/camunda/worker/akka/worker/Worker.scala
@@ -10,29 +10,40 @@ import scala.util.{Success, Failure}
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 
+/**
+ * Worker that can scheduled by PollActor.
+ */
 trait Worker extends Actor with ActorLogging {
     
-  val consumerId: String = getNameOfActor
+  // consumerId should be the same as for poll&lock request
+  val consumerId: String = Worker.getNameOfActor(self)
   
   def receive = {
     case task: LockedTask => 
       
-      log.debug(s"execute task '$task'")  
-      val manager = sender
+      log.debug(s"execute task '$task'")
+      // note that sender is not available in future
+      val caller = sender
       
+      // execute the task async
       work(task) onComplete {
-         case Success(variables)  => manager ! Complete(consumerId, task.id, variables)
-         case Failure(cause)      => manager ! FailedTask(consumerId, task.id, cause.getMessage)
+         case Success(variables)  => caller ! Complete(consumerId, task.id, variables)
+         case Failure(cause)      => caller ! FailedTask(consumerId, task.id, cause.getMessage)
       }
   }
   
+  // should implement by the concrete worker
   def work(task: LockedTask): Future[Map[String, VariableValue]]
   
-  private def getNameOfActor = {
-    val name = self.path.name
+}
+
+object Worker {
+  
+  def getNameOfActor(actor: ActorRef): String = {
+    val name = actor.path.name
     if(name.startsWith("$")) {
       // in case of a worker of router
-      self.path.parent.name
+      actor.path.parent.name
     } else {
       name
     }
